11b36f3bb4a79f0c3190a8402d1f4ac0f399cbdc
Julius Charles <cjmatheka@gmail.com>
2025-11-18 00:01:24 +0300

    Update portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,6 +20,7 @@ export default function RootLayout({
         className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
